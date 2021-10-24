f325b4e59837197ed6d9cb7a70e6ab9a6e485993
Lasse Hjorth Madsen <lassehjorthmadsen@gmail.com>
2021-10-24 21:07:14 +0200

    work on DK chart

--- sommertid.R
+++ sommertid.R
@@ -2,12 +2,14 @@ library(tidyverse)
 library(ggmap)
 library(suncalc)
 library(lubridate)
-library(RColorBrewer)
+library(RColorBrewer) 
 
 theme_set(theme_minimal())
 
 register_google(key = Sys.getenv("GL_API_KEY"))
+cph <- geocode("Copenhagen")
 
+# Values
 year_start   <- as_date("2021-01-01")
 year_end     <- as_date("2021-12-31")
 get_up_at    <- as.POSIXct("06:30:00", format = "%H:%M:%S")
@@ -16,8 +18,7 @@ summer_start <- as_date("2021-03-28")
 summer_end   <- as_date("2021-10-31")
 day_start    <- as.POSIXct("03:00:00", format = "%H:%M:%S")
 day_end      <- as.POSIXct("23:30:00", format = "%H:%M:%S")
-
-cph <- geocode("Copenhagen")
+day_mid      <- as.POSIXct("12:00:00", format = "%H:%M:%S")
 
 df <- tibble(date = seq(year_start, year_end, by="days"),
              lat = cph$lat, lon = cph$lon, tz ="CET") 
@@ -48,6 +49,109 @@ sun <- getSunlightTimes(data = df) %>%
   ))
 
 
+# My colors
+day_color    <- brewer.pal(8, name = "YlOrRd")[3]
+diff_color   <- brewer.pal(8, name = "YlOrRd")[4]
+sleep_color <- gray(level = 0, alpha = 0.05)
+
+pl <- sun %>%
+  mutate(fill = day_color) %>%
+  ggplot(aes(x = date)) +
+  geom_ribbon(aes(ymin = sunrise, ymax = sunset, fill = "Dagslys"), show.legend = T) +
+  geom_ribbon(aes(ymin = sunrise_raw, ymax = sunrise, fill = "Flyttet dagslys")) +
+  geom_ribbon(aes(ymin = sunset_raw, ymax = sunset), fill = diff_color) +
+  scale_fill_manual(values = c(day_color, diff_color)) +
+  scale_y_datetime(
+    date_breaks = "2 hours",
+    date_labels = "%H:%M",
+    limits = c(day_start, day_end),
+    expand = c(0, 0)
+  ) +
+  scale_x_date(
+    date_breaks = "months",
+    date_minor_breaks = "month",
+    date_labels = "%B",
+    expand = c(0, 0)
+  ) +
+  labs(y = "Klokken", x = "Måned") +
+  annotate(
+    "rect",
+    xmin = year_start,
+    xmax = year_end,
+    ymin = day_start,
+    ymax = get_up_at,
+    fill = sleep_color
+  ) +
+  annotate(
+    "rect",
+    xmin = year_start,
+    xmax = year_end,
+    ymin = to_bed_at,
+    ymax = day_end,
+    fill = sleep_color
+  ) +
+  annotate(
+    "text",
+    x = year_start + days(5),
+    y = day_start + minutes(15),
+    label = "Mange sover\ni dette tidsrum",
+    hjust = 0,
+    vjust = 0,
+    size = 3
+  ) +
+  annotate(
+    "text",
+    x = year_start + days(5),
+    y = day_mid,
+    label = "Sommetid flytter soltimer\nfra tidlig morgen til aften",
+    hjust = 0,
+    vjust = 0,
+    size = 3
+  ) +
+  geom_curve(
+    x = year_start + weeks(11),
+    y = day_mid - minutes(30),
+    xend = summer_start,
+    yend = day_start + minutes(200),
+    arrow = arrow(length = unit(1.5, "mm"), type = "closed")
+  ) + 
+  geom_curve(
+    x = year_start + weeks(21),
+    y = day_mid + minutes(15),
+    xend = summer_end,
+    yend = day_mid + minutes(260),
+    arrow = arrow(length = unit(1.5, "mm"), type = "closed")
+  ) +
+theme(
+  legend.position = c(0.95, 0.95),
+  legend.justification = c(1, 1),
+  legend.title = element_blank(),
+  legend.text = element_text(size = 8),
+  legend.key.size = unit(1, "line")
+)
+
+
+pl
+pl + geom_curve(
+  x = year_start + weeks(11),
+  y = day_mid - minutes(30),
+  xend = summer_start,
+  yend = day_start + minutes(200),
+  arrow = arrow(length = unit(1.5, "mm"), type = "closed")
+)
+
+pl + geom_curve(
+  x = year_start + weeks(21),
+  y = day_mid,
+  xend = summer_end,
+  yend = day_mid + minutes(250),
+  arrow = arrow(length = unit(1.5, "mm"), type = "closed")
+)
+
+pl + ylim(day_start, day_end)
+pl + coord_cartesian(ylim = c(day_start, day_end))
+
+# Discarded versions
 morning <- tibble(
   x = c(year_start, summer_start, summer_start, summer_end, summer_end),
   y = c(get_up_at, get_up_at, get_up_at - hours(1), get_up_at - hours(1), get_up_at),
@@ -71,20 +175,3 @@ sun %>%
   geom_segment(data = morning, aes(x = x, y = y, xend = xend, yend = yend)) +
   geom_segment(data = evening, aes(x = x, y = y, xend = xend, yend = yend)) +
   labs(y = "Normaltid")
-
-
-day_color    <- brewer.pal(8, name = "YlOrRd")[3]
-diff_color   <- brewer.pal(8, name = "YlOrRd")[4]
-
-
-sun %>% 
-  ggplot(aes(x = date)) +
-  geom_ribbon(aes(ymin = sunrise, ymax = sunset), fill = day_color) +
-  geom_ribbon(aes(ymin = sunrise_raw, ymax = sunrise), fill = diff_color) +
-  geom_ribbon(aes(ymin = sunset_raw, ymax = sunset), fill = diff_color) +
-  scale_y_datetime(date_breaks = "2 hours", date_labels = "%H:%M", 
-                   limits = c(day_start, day_end)) +
-  scale_x_date(date_breaks = "months", date_minor_breaks = "month", date_labels = "%B") +
-  geom_segment(x = year_start, y = get_up_at, xend = year_end, yend = get_up_at, color = "darkblue") +
-  geom_segment(x = year_start, y = to_bed_at, xend = year_end, yend = to_bed_at, color = "darkblue") +
-  labs(y = "Klokkeslet", x = "Måned")
